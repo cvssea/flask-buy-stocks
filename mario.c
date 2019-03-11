490d8ba081121a812a1ccc5e4ef11154ad137b79
cvssea <catalin.varvaroi@gmail.com@users.noreply.github.com>
2019-03-11 22:18:57 +0000

    20190311T221824Z

---
+++ mario.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void mario(int);
+
+int main(void)
+{    
+    int height = get_int("Enter height: ");
+    while (height < 1) {
+        height = get_int("Enter height: ");
+    }
+    mario(height);  
+}
+
+void mario(int height) {
+    for (int i = 1; i <= height; i++) {
+        for (int j = height; j > 0; j--) {
+            j <= i ? printf("#") : printf(" ");
+        }
+        printf("\n");
+    }
+}
